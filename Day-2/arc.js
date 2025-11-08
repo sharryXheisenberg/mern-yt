8f7b00824186197de68ab49451bcdc1b018c2214
sharryXheisenberg <pandesaurabh4596@gmail.com>
2025-11-08 21:36:02 +0530

    Updated learning of Day-2

--- Day-2/arc.js
+++ Day-2/arc.js
@@ -11,3 +11,27 @@ console.log("Hello world 1");
 // 1) top level project(console)
 // 2) setTimeout
 // 3) SetImmediate
+// 4) callbacks and rejected callbacks
+let start = Date.now();
+//CPU intensive tasks
+process.env.UV_THREAD_POOL_SIZE = 5;
+const crypto = require("crypto");
+crypto.pbkdf2("password-1","salt1",100000,1024,"sha512",()=>{
+    console.log(`${Date.now() - start}ms Done`);
+})
+
+crypto.pbkdf2("password-1","salt1",100000,1024,"sha512",()=>{
+    console.log(`${Date.now() - start}ms Done`);
+})
+
+crypto.pbkdf2("password-1","salt1",100000,1024,"sha512",()=>{
+    console.log(`${Date.now() - start}ms Done`);
+})
+
+crypto.pbkdf2("password-1","salt1",100000,1024,"sha512",()=>{
+    console.log(`${Date.now() - start}ms Done`);
+})  
+
+crypto.pbkdf2("password-1","salt1",100000,1024,"sha512",()=>{
+    console.log(`${Date.now() - start}ms Done`);
+})
