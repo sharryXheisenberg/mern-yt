839a47fcee10481f466d86842f72fead89820e4f
sharryXheisenberg <pandesaurabh4596@gmail.com>
2025-11-27 10:01:39 +0530

    Updated learning from the streams

--- Day-4/stream.js
+++ Day-4/stream.js
@@ -15,5 +15,15 @@ readableStream.on("data",(chunk)=>{
     writeableStream.write(chunk);
 });
 
+const readbleStreamNew = new Readable({
+    read(){},
+})
+
+const writeableStreamNew = new Writable({
+    write(streamData){
+        console.log("Writing .." , streamData);
+    },
+})
 
 console.log(readableStream.push("Hello"));
+console.log(readbleStreamNew.push("OP bro!"));
