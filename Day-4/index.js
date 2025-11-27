863d1c9e0859549c5aa984bb5857063f03be6d50
sharryXheisenberg <pandesaurabh4596@gmail.com>
2025-11-27 10:01:00 +0530

    Updated learning from the read and write stream for end() method

--- Day-4/index.js
+++ Day-4/index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const fs= require("fs");
+const path = require("path");
 
 
 const server = http.createServer((req,res)=>{
@@ -21,21 +22,49 @@ const server = http.createServer((req,res)=>{
     // readableStream.pipe(res)
 
 
-    const file = fs.readFileSync("sample.txt");
-    fs.writeFileSync("output.txt",file);
-    res.end();
+    // const file = fs.readFileSync("sample.txt");
+    // fs.writeFileSync("output.txt",file);
+    // res.end();
 
     // res.end();
 
     // copying file from one file to another in streams way
 
-const readStream = fs.createReadStream("sample.txt");
-const writeStream = fs.createWriteStream("output.txt");
+// const readStream = fs.createReadStream("sample.txt");
+// const writeStream = fs.createWriteStream("output.txt");
 
-readStream.on("data",(chunk)=>{
-    console.log("CHUNK:",chunk);
-    writeStream.write(chunk);
-})
+// readStream.on("data",(chunk)=>{
+//     console.log("CHUNK:",chunk);
+//     writeStream.write(chunk);
+// })
+
+
+
+// ?................3 ----------Stream processing (transforming)
+// convert all words to uppercase
+// replace epsum words to saurabh
+const readStreamNew = fs.createReadStream(path.join(__dirname,"sample.txt"));
+const writeStreamNew = fs.createWriteStream(path.join(__dirname,"output.txt"));
+
+
+// bad approach 
+
+
+readStreamNew.on("data",(chunk)=>{
+    const modifiedWords = chunk.toString().toUpperCase().replaceAll(/ipsum/gi , "Saurabh");
+    writeStreamNew.write(modifiedWords);
+});
+
+readStreamNew.on("end",()=>{
+    writeStreamNew.end();
+    res.end("file processed!");
+});
+readStreamNew.on("error", (err) => {
+        console.error("Read error:", err);
+        res.end("Error reading file!");
+    });
+
+
 
 });
 
