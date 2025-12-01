16b5db36d6dfa401053b8c7928a8d61890afe3e7
sharryXheisenberg <pandesaurabh4596@gmail.com>
2025-12-01 22:04:31 +0530

    Data for retrieval

---
+++ ExpressJS/Day-1/Data/data.js
@@ -0,0 +1,27 @@
+export const data = [
+    {
+        id:1,
+        name:"John",
+        displayName:"John Doe"
+    },
+    {
+        id:2,
+        name:"Jane",
+        displayName:"Jane Ross"
+    },
+    {
+        id:3,
+        name:"Jax",
+        displayName:"Jax Tyler"
+    },
+    {
+        id:4,
+        name:"Mike",
+        displayName:"Mike ownes"
+    },
+    {
+        id:5,
+        name:"Sharry",
+        displayName:"Sharry Paul"
+    },
+]
