39b3beeb7a9d6a658ddeda95c0bba80697a01523
sharryXheisenberg <pandesaurabh4596@gmail.com>
2025-12-20 21:52:58 +0530

    Updated task-manager and sessions learning

---
+++ ExpressJS/mini_proj/task-manager/src/server.js
@@ -0,0 +1,33 @@
+// global imports 
+import express from 'express';
+import session  from 'express-session';
+import authRoute from './routes/auth.routes.js';
+
+const app = express();
+const PORT = 8080;
+app.use(express.json());
+app.use(session({
+    secret:"secret-key",  //this acts a string val for creating req.signed cookies or in easy words HMVAC key
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        httpOnly:true,
+        maxAge:1000*60*60*24 // 1 day
+    }
+}))
+
+
+app.get('/',(req,res)=>{
+    res.send('Welcome to task Manager');
+})
+
+app.use("/auth",authRoute);
+
+app.listen(PORT,(req,res)=>{
+    console.log(`Server is running on ${PORT}`);
+})
+
+
+
+
+//Local imports 
