c1485020d194c54c6db72481a293a0cab962e016
sharryXheisenberg <pandesaurabh4596@gmail.com>
2025-12-27 08:38:11 +0530

    Updated task routes and all

--- ExpressJS/mini_proj/task-manager/src/routes/task.routes.js
+++ ExpressJS/mini_proj/task-manager/src/routes/task.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { authMiddleware } from "../../middlewares/auth.middleware";
-import { getAllTask,createTask,updateTask,deleteTask } from "../controller/task.controller";
+import { authMiddleware } from "../../middlewares/auth.middleware.js";
+import { getAllTask,createTask,updateTask,deleteTask } from "../controller/task.controller.js";
 
 const router = Router();
 
